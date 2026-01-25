8c3cf41b6564ff62c386ddb5b282d2225a765a3d
Rak <antmag0916@gmail.com>
2026-01-25 17:44:41 -0500

    Added blank functions for class port

--- portClass.py
+++ portClass.py
@@ -1,3 +1,26 @@
-x=4
-y=3
-z=3
+class port:
+    def __init__(self, acronym, portNum, spelledOut, desc):
+        #pass = null operator
+        self.acronym = acronym
+        self.portNum = portNum
+        self.spelledOut = spelledOut
+        self.desc = desc
+
+    def getAcronym():
+        pass
+    def setAcronum():
+        pass
+    def getPortNum():
+        pass
+    def setPortNum():
+        pass
+    def getSpelledOut():
+        pass
+    def setSpelledOut():
+        pass
+    def getDesc():
+        pass
+    def setDesc():
+        pass
+    
+    
