368eaa0f53adb32d478d64bdb3f38a16e15e3e61
Rak <antmag0916@gmail.com>
2026-01-26 18:39:25 -0500

    complete setters and getters

--- portClass.py
+++ portClass.py
@@ -13,19 +13,19 @@ class port:
     #the return f thing allow you to put {} and variables in stuff
     def getAcronym(self):
         return f"{self.__acronym}"
-    def setAcronum():
-        pass
-    def getPortNum():
-        pass
-    def setPortNum():
-        pass
-    def getSpelledOut():
-        pass
-    def setSpelledOut():
-        pass
-    def getDesc():
-        pass
-    def setDesc():
-        pass
+    def _setAcronum(self, newAcronum):
+        self.__acronym = newAcronum
+    def getPortNum(self):
+        return f"{self.__portNum}"
+    def _setPortNum(self, newPortNum):
+        self.__portNum = newPortNum
+    def getSpelledOut(self):
+        return f"{self.__spelledOut}"
+    def _setSpelledOut(self, newSpelledOut):
+        self.__spelledOut = newSpelledOut
+    def getDesc(self):
+        return f"{self.__desc}"
+    def _setDesc(self, newDesc):
+        self.__desc = newDesc
     
     
