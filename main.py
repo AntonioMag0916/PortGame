ad378b80a70e326cb79b93ed512639b2aae8786f
Rak <antmag0916@gmail.com>
2026-01-26 21:04:51 -0500

    Added a few ports

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ import portClass
 http = portClass.port("HTTP", 80, "Hypertext Transfer Protocol", "TCP","Websites, insecure", True)
 https = portClass.port("HTTPS", 443, "Hypertext Transfer Protocol Secure", "TCP", "Websites, secure", True)
 smtp = portClass.port("SMTP", 25, "Simple Mail Transfer Protocl", "TCP", "Mail deilvery protocol", True)
-telnet = portClass.port("Telnet")
+telnet = portClass.port("Telnet", 23, "Telent", "TCP", "Remote access protocol, insecure", False)
+ssh = portClass.port("SSH", 22, "Secure Shell", "TCP/UDP", "Remote access protocol, secure", True)
+dns = portClass.port("DNS", 53, "Domain Name System", "TCP/UDP", "IPs to human readable text", True)
+mysql = portClass.port("MySQL", 3306, "MySQL", "TCP", "SQL Server", False)
+ftpdata = portClass.port("FTP-Data", 20, "FTP-Data", "TCP/UDP", "FTP download and upload service", False)
+ftp = portClass.port("FTP", 21, "File Transfer Protocol", "TCP/UDP", "Allows for a stateful channel for FTP commands", True)
+syslog = portClass.port("Syslog", 514, "System Logging", "UDP", "Enables centralized collection of logs and events", True)
+ntp = portClass.port("NTP", 123, "Network Time Protocol", "UDP", "Automatic time update protocol", True)
+
 
 #ftpClient
 #ftpServer
 #ssh
-#telnet
 #pop3
 #snmp
 #imaps
